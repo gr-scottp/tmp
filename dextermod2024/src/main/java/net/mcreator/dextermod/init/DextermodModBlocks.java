ba1d072249a2d36b9f07f14d0d12b1e75c285c35
codeclub202318 <codeclub202318@outlook.com>
2024-09-12 16:58:17 +0100

    I made a block

---
+++ dextermod2024/src/main/java/net/mcreator/dextermod/init/DextermodModBlocks.java
@@ -0,0 +1,20 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.dextermod.init;
+
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.DeferredHolder;
+
+import net.minecraft.world.level.block.Block;
+
+import net.mcreator.dextermod.block.DexterFaceBlockBlock;
+import net.mcreator.dextermod.DextermodMod;
+
+public class DextermodModBlocks {
+	public static final DeferredRegister.Blocks REGISTRY = DeferredRegister.createBlocks(DextermodMod.MODID);
+	public static final DeferredHolder<Block, Block> DEXTER_FACE_BLOCK = REGISTRY.register("dexter_face_block", DexterFaceBlockBlock::new);
+	// Start of user code block custom blocks
+	// End of user code block custom blocks
+}
---
+++ dextermod2024/src/main/java/net/mcreator/dextermod/init/DextermodModTabs.java
@@ -0,0 +1,28 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.dextermod.init;
+
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.core.registries.Registries;
+
+import net.mcreator.dextermod.DextermodMod;
+
+@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
+public class DextermodModTabs {
+	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, DextermodMod.MODID);
+
+	@SubscribeEvent
+	public static void buildTabContentsVanilla(BuildCreativeModeTabContentsEvent tabData) {
+		if (tabData.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+			tabData.accept(DextermodModBlocks.DEXTER_FACE_BLOCK.get().asItem());
+		}
+	}
+}
