31d986cc594f1a795814214a390d0d3238e4b7cd
codeclub20236 <134069199+codeclub20236@users.noreply.github.com>
2024-10-03 16:42:25 +0100

    we doid coding
    we doid coding

---
+++ wingsoffire/src/main/java/net/mcreator/wingsoffire/procedures/WingsOfFireexOnBlockRightClickedProcedure.java
@@ -0,0 +1,12 @@
+package net.mcreator.wingsoffire.procedures;
+
+public class WingsOfFireexOnBlockRightClickedProcedure {
+	public static void execute(double x, double y, double z) {
+		double anikax = 0;
+		double anikay = 0;
+		double anikaz = 0;
+		anikax = x;
+		anikay = y;
+		anikaz = z;
+	}
+}
