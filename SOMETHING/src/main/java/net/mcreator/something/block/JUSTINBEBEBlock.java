651f60bc1059696736bc336e24fbc1ef1ab3e762
codeclub20232 <134067681+codeclub20232@users.noreply.github.com>
2024-10-03 16:40:40 +0100

    i DID IT

--- SOMETHING/src/main/java/net/mcreator/something/block/JUSTINBEBEBlock.java
+++ SOMETHING/src/main/java/net/mcreator/something/block/JUSTINBEBEBlock.java
@@ -3,6 +3,7 @@ package net.mcreator.something.block;
 
 import org.checkerframework.checker.units.qual.s;
 
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -10,10 +11,14 @@ import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
 import net.mcreator.something.procedures.JUSTINBEBERedstoneOnProcedure;
+import net.mcreator.something.procedures.JUSTINBEBEOnBlockRightClickedProcedure;
 import net.mcreator.something.procedures.JUSTINBEBEEntityWalksOnTheBlockProcedure;
 
 public class JUSTINBEBEBlock extends Block {
@@ -38,5 +43,19 @@ public class JUSTINBEBEBlock extends Block {
 	public void stepOn(Level world, BlockPos pos, BlockState blockstate, Entity entity) {
 		super.stepOn(world, pos, blockstate, entity);
 		JUSTINBEBEEntityWalksOnTheBlockProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ(), entity);
+	}
+
+	@Override
+	public InteractionResult useWithoutItem(BlockState blockstate, Level world, BlockPos pos, Player entity, BlockHitResult hit) {
+		super.useWithoutItem(blockstate, world, pos, entity, hit);
+		int x = pos.getX();
+		int y = pos.getY();
+		int z = pos.getZ();
+		double hitX = hit.getLocation().x;
+		double hitY = hit.getLocation().y;
+		double hitZ = hit.getLocation().z;
+		Direction direction = hit.getDirection();
+		JUSTINBEBEOnBlockRightClickedProcedure.execute(x, y, z);
+		return InteractionResult.SUCCESS;
 	}
 }
---
+++ SOMETHING/src/main/java/net/mcreator/something/procedures/JUSTINBEBEOnBlockRightClickedProcedure.java
@@ -0,0 +1,12 @@
+package net.mcreator.something.procedures;
+
+public class JUSTINBEBEOnBlockRightClickedProcedure {
+	public static void execute(double x, double y, double z) {
+		double Xcurrent = 0;
+		double Ycurrent = 0;
+		double zcurrent = 0;
+		Xcurrent = x;
+		Ycurrent = y;
+		zcurrent = z;
+	}
+}
