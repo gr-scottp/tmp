711fb1d968f4d435eeae24516631c75cc3c778e9
codeclub202319 <134315418+codeclub202319@users.noreply.github.com>
2025-02-27 16:56:53 +0000

    helo

--- cavarli904/src/main/java/net/mcreator/cavarli/procedures/CavarliBlockOnBlockRightClickedProcedure.java
+++ cavarli904/src/main/java/net/mcreator/cavarli/procedures/CavarliBlockOnBlockRightClickedProcedure.java
@@ -3,7 +3,6 @@ package net.mcreator.cavarli.procedures;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.core.BlockPos;
-importnet
 
 import net.mcreator.cavarli.init.CavarliModBlocks;
 
@@ -11,7 +10,5 @@ public class CavarliBlockOnBlockRightClickedProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
 		world.setBlock(BlockPos.containing(x + 1, y, z), CavarliModBlocks.CAVARLI_BLOCK.get().defaultBlockState(), 3);
 		world.setBlock(BlockPos.containing(x, y, z), Blocks.TNT.defaultBlockState(), 3);
-		world.playSound(
-				null,
-				new BlockPos(x,z,y
-						
+	}
+}
