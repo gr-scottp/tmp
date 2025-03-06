0c866e1b74943f71975dbedab73628d728b771c7
codeclub20234 <134068441+codeclub20234@users.noreply.github.com>
2025-03-06 17:00:11 +0000

    REHHHEHEHHEHEHEHHEHEHEHHHHHHHHHHHHHHHHHHHHH OHHHHHHHHHHHHHHHHHHHHHUEHKUEGJHGKSGYUKHJDYRITYU:OHLJVCMFKTYUO:ILJK<BVMDJTRIUHL:KJBXFZJYU:HGKF

--- OogaBoogaBlockGen-SophiaA/src/main/java/net/mcreator/oogaboogablockgen/item/HyperventalationSamsungRefridgerationWandItem.java
+++ OogaBoogaBlockGen-SophiaA/src/main/java/net/mcreator/oogaboogablockgen/item/HyperventalationSamsungRefridgerationWandItem.java
@@ -2,18 +2,13 @@
 package net.mcreator.oogaboogablockgen.item;
 
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.ShovelItem;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.DiggerItem;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.tags.TagKey;
 import net.minecraft.tags.BlockTags;
 
@@ -54,13 +49,6 @@ public class HyperventalationSamsungRefridgerationWandItem extends ShovelItem {
 
 	public HyperventalationSamsungRefridgerationWandItem() {
 		super(TOOL_TIER, new Item.Properties().attributes(DiggerItem.createAttributes(TOOL_TIER, 13f, -3.9f)).fireResistant());
-	}
-
-	@Override
-	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
-		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
-		OGBWandRightclickedProcedure.execute(world, entity.getX(), entity.getY(), entity.getZ());
-		return ar;
 	}
 
 	@Override
