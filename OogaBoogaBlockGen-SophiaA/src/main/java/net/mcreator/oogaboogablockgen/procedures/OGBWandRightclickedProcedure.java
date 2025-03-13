176f2b140968abad23da905ae5780007f0db6af2
codeclub202316 <134312470+codeclub202316@users.noreply.github.com>
2025-03-13 17:02:17 +0000

    fwefrwfgfhgt4werfgtju6iy54te

--- OogaBoogaBlockGen-SophiaA/src/main/java/net/mcreator/oogaboogablockgen/procedures/OGBWandRightclickedProcedure.java
+++ OogaBoogaBlockGen-SophiaA/src/main/java/net/mcreator/oogaboogablockgen/procedures/OGBWandRightclickedProcedure.java
@@ -13,19 +13,41 @@ public class OGBWandRightclickedProcedure {
 		double Z1Offset = 0;
 		double X2Offset = 0;
 		double Z2Offset = 0;
-		ZOffset = -1;
-		XOffset = -1;
-		YOffset = -1;
-		for (int index0 = 0; index0 < 10; index0++) {
-			for (int index1 = 0; index1 < 10; index1++) {
-				for (int index2 = 0; index2 < 10; index2++) {
-					world.setBlock(BlockPos.containing(x + XOffset, y + 1, z + ZOffset), Blocks.OAK_PLANKS.defaultBlockState(), 3);
-					XOffset = XOffset + 1;
-				}
+		ZOffset = -2;
+		XOffset = -2;
+		YOffset = 0;
+		for (int index0 = 0; index0 < 6; index0++) {
+			for (int index1 = 0; index1 < 6; index1++) {
+				world.setBlock(BlockPos.containing(x + XOffset, y + 0, z + ZOffset), Blocks.OAK_PLANKS.defaultBlockState(), 3);
+				world.setBlock(BlockPos.containing(x + XOffset, y + 5, z + ZOffset), Blocks.SMOOTH_QUARTZ.defaultBlockState(), 3);
 				XOffset = XOffset + 1;
 			}
 			ZOffset = ZOffset + 1;
-			XOffset = -1;
+			XOffset = -2;
+		}
+		ZOffset = -2;
+		XOffset = -2;
+		YOffset = 0;
+		for (int index2 = 0; index2 < 5; index2++) {
+			for (int index3 = 0; index3 < 6; index3++) {
+				world.setBlock(BlockPos.containing(x + XOffset, y + YOffset, z + 3), Blocks.QUARTZ_PILLAR.defaultBlockState(), 3);
+				world.setBlock(BlockPos.containing(x + XOffset, y + YOffset, z - 3), Blocks.QUARTZ_PILLAR.defaultBlockState(), 3);
+				XOffset = XOffset + 1;
+			}
+			YOffset = YOffset + 1;
+			XOffset = -2;
+		}
+		ZOffset = -2;
+		XOffset = -2;
+		YOffset = 0;
+		for (int index4 = 0; index4 < 5; index4++) {
+			for (int index5 = 0; index5 < 6; index5++) {
+				world.setBlock(BlockPos.containing(x + 3, y + YOffset, z + ZOffset), Blocks.QUARTZ_PILLAR.defaultBlockState(), 3);
+				world.setBlock(BlockPos.containing(x - 3, y + YOffset, z + ZOffset), Blocks.QUARTZ_PILLAR.defaultBlockState(), 3);
+				XOffset = XOffset + 1;
+			}
+			YOffset = YOffset + 1;
+			XOffset = -2;
 		}
 	}
 }
