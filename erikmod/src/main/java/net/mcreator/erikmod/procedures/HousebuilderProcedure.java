e1d1f1c9f8a361553d4e7efdfeb5137976fbd7ea
codeclub20233 <codeclub20233@outlook.com>
2025-03-13 17:01:24 +0000

    i made a house

--- erikmod/src/main/java/net/mcreator/erikmod/procedures/HousebuilderProcedure.java
+++ erikmod/src/main/java/net/mcreator/erikmod/procedures/HousebuilderProcedure.java
@@ -9,31 +9,40 @@ public class HousebuilderProcedure {
 		double xoffset = 0;
 		double zoffset = 0;
 		double yoffset = 0;
-		xoffset = -1;
-		zoffset = -1;
-		yoffset = -1;
-		for (int index0 = 0; index0 < 3; index0++) {
-			for (int index1 = 0; index1 < 3; index1++) {
-				world.setBlock(BlockPos.containing(xoffset + x, y - 1, zoffset + z), Blocks.STRIPPED_BIRCH_WOOD.defaultBlockState(), 3);
+		xoffset = -2;
+		zoffset = 0;
+		yoffset = -2;
+		for (int index0 = 0; index0 < 5; index0++) {
+			for (int index1 = 0; index1 < 5; index1++) {
+				world.setBlock(BlockPos.containing(x + xoffset, y - 1, z + zoffset), Blocks.STRIPPED_BIRCH_WOOD.defaultBlockState(), 3);
+				world.setBlock(BlockPos.containing(x + xoffset, y + 3, z + zoffset), Blocks.SPRUCE_PLANKS.defaultBlockState(), 3);
 				xoffset = xoffset + 1;
 			}
-			xoffset = -1;
+			xoffset = -2;
 			zoffset = zoffset + 1;
 		}
+		xoffset = -2;
+		zoffset = 0;
+		yoffset = -2;
 		for (int index2 = 0; index2 < 3; index2++) {
-			for (int index3 = 0; index3 < 3; index3++) {
-				world.setBlock(BlockPos.containing(xoffset + x, y - 1, zoffset + z), Blocks.STRIPPED_BIRCH_WOOD.defaultBlockState(), 3);
+			for (int index3 = 0; index3 < 5; index3++) {
+				world.setBlock(BlockPos.containing(xoffset + x, y + yoffset, z + 2), Blocks.STRIPPED_BIRCH_WOOD.defaultBlockState(), 3);
+				world.setBlock(BlockPos.containing(xoffset + x, y + yoffset, z - 2), Blocks.SPRUCE_PLANKS.defaultBlockState(), 3);
 				xoffset = xoffset + 1;
 			}
-			xoffset = -1;
+			zoffset = zoffset + 1;
 			zoffset = zoffset + 1;
 		}
+		xoffset = -2;
+		zoffset = 0;
+		yoffset = -2;
 		for (int index4 = 0; index4 < 3; index4++) {
-			for (int index5 = 0; index5 < 3; index5++) {
-				world.setBlock(BlockPos.containing(xoffset + x, y - 1, zoffset + z), Blocks.STRIPPED_BIRCH_WOOD.defaultBlockState(), 3);
+			for (int index5 = 0; index5 < 5; index5++) {
+				world.setBlock(BlockPos.containing(x + 2, y + yoffset, z + zoffset), Blocks.STRIPPED_BIRCH_WOOD.defaultBlockState(), 3);
+				world.setBlock(BlockPos.containing(x - 2, y + yoffset, z - zoffset), Blocks.SPRUCE_PLANKS.defaultBlockState(), 3);
 				xoffset = xoffset + 1;
 			}
-			xoffset = -1;
+			zoffset = -2;
 			zoffset = zoffset + 1;
 		}
 	}
