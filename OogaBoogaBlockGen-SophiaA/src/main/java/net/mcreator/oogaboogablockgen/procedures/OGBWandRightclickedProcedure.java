904e4491e8d7580710582445cf297af6c78e6e56
codeclub202310 <codeclub202310@outlook.com>
2025-03-20 17:00:23 +0000

    rrHEHJGFGHCYUDWHQKL>GTULYIQUR>K

--- OogaBoogaBlockGen-SophiaA/src/main/java/net/mcreator/oogaboogablockgen/procedures/OGBWandRightclickedProcedure.java
+++ OogaBoogaBlockGen-SophiaA/src/main/java/net/mcreator/oogaboogablockgen/procedures/OGBWandRightclickedProcedure.java
@@ -1,5 +1,8 @@
 package net.mcreator.oogaboogablockgen.procedures;
 
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.core.BlockPos;
@@ -16,38 +19,47 @@ public class OGBWandRightclickedProcedure {
 		ZOffset = -2;
 		XOffset = -2;
 		YOffset = 0;
-		for (int index0 = 0; index0 < 6; index0++) {
-			for (int index1 = 0; index1 < 6; index1++) {
+		for (int index0 = 0; index0 < 5; index0++) {
+			for (int index1 = 0; index1 < 5; index1++) {
 				world.setBlock(BlockPos.containing(x + XOffset, y + 0, z + ZOffset), Blocks.OAK_PLANKS.defaultBlockState(), 3);
-				world.setBlock(BlockPos.containing(x + XOffset, y + 5, z + ZOffset), Blocks.SMOOTH_QUARTZ.defaultBlockState(), 3);
+				world.setBlock(BlockPos.containing(x + XOffset, y + 3, z + ZOffset), Blocks.SMOOTH_QUARTZ.defaultBlockState(), 3);
 				XOffset = XOffset + 1;
 			}
 			ZOffset = ZOffset + 1;
 			XOffset = -2;
 		}
-		ZOffset = -2;
 		XOffset = -2;
 		YOffset = 0;
-		for (int index2 = 0; index2 < 5; index2++) {
-			for (int index3 = 0; index3 < 6; index3++) {
-				world.setBlock(BlockPos.containing(x + XOffset, y + YOffset, z + 3), Blocks.QUARTZ_PILLAR.defaultBlockState(), 3);
-				world.setBlock(BlockPos.containing(x + XOffset, y + YOffset, z - 3), Blocks.QUARTZ_PILLAR.defaultBlockState(), 3);
-				XOffset = XOffset + 1;
+		for (int index2 = 0; index2 < 3; index2++) {
+			for (int index3 = 0; index3 < 5; index3++) {
+				world.setBlock(BlockPos.containing(x + XOffset, y + YOffset, z + 2), Blocks.POLISHED_ANDESITE.defaultBlockState(), 3);
+				world.setBlock(BlockPos.containing(x + XOffset, y + YOffset, z - 2), Blocks.QUARTZ_PILLAR.defaultBlockState(), 3);
+				ZOffset = XOffset + 1;
 			}
 			YOffset = YOffset + 1;
 			XOffset = -2;
 		}
 		ZOffset = -2;
-		XOffset = -2;
 		YOffset = 0;
-		for (int index4 = 0; index4 < 5; index4++) {
-			for (int index5 = 0; index5 < 6; index5++) {
-				world.setBlock(BlockPos.containing(x + 3, y + YOffset, z + ZOffset), Blocks.QUARTZ_PILLAR.defaultBlockState(), 3);
-				world.setBlock(BlockPos.containing(x - 3, y + YOffset, z + ZOffset), Blocks.QUARTZ_PILLAR.defaultBlockState(), 3);
+		for (int index4 = 0; index4 < 3; index4++) {
+			for (int index5 = 0; index5 < 5; index5++) {
+				world.setBlock(BlockPos.containing(x + 2, y + YOffset, z + ZOffset), Blocks.QUARTZ_PILLAR.defaultBlockState(), 3);
+				world.setBlock(BlockPos.containing(x - 2, y + YOffset, z + ZOffset), Blocks.QUARTZ_PILLAR.defaultBlockState(), 3);
 				XOffset = XOffset + 1;
 			}
-			YOffset = YOffset + 1;
+			YOffset = YOffset + 0;
 			XOffset = -2;
 		}
+		world.setBlock(BlockPos.containing(x + 1, y, z + 2), Blocks.LIGHT_GRAY_STAINED_GLASS_PANE.defaultBlockState(), 3);
+		world.setBlock(BlockPos.containing(x + 2, y + 2, z + 2), Blocks.LIGHT_GRAY_STAINED_GLASS_PANE.defaultBlockState(), 3);
+		world.setBlock(BlockPos.containing(x - 1, y, z + 2), Blocks.AIR.defaultBlockState(), 3);
+		world.setBlock(BlockPos.containing(x - 1, y + 1, z + 2), Blocks.AIR.defaultBlockState(), 3);
+		world.setBlock(BlockPos.containing(x - 1, y + 1, z + 2), (new Object() {
+			public BlockState with(BlockState _bs, String _property, String _newValue) {
+				Property<?> _prop = _bs.getBlock().getStateDefinition().getProperty(_property);
+				return _prop instanceof EnumProperty _ep && _ep.getValue(_newValue).isPresent() ? _bs.setValue(_ep, (Enum) _ep.getValue(_newValue).get()) : _bs;
+			}
+		}.with(Blocks.BAMBOO_DOOR.defaultBlockState(), "half", "upper")), 3);
+		world.setBlock(BlockPos.containing(x - 1, y, z + 2), Blocks.BAMBOO_DOOR.defaultBlockState(), 3);
 	}
 }
