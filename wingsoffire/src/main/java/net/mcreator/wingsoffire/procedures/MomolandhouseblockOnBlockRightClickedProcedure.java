c83a580a60a440f0e408d0a8a4b4b4e28bb52d34
codeclub202319 <134315418+codeclub202319@users.noreply.github.com>
2025-03-20 17:02:18 +0000

    i made a hsouse yaya

--- wingsoffire/src/main/java/net/mcreator/wingsoffire/procedures/MomolandhouseblockOnBlockRightClickedProcedure.java
+++ wingsoffire/src/main/java/net/mcreator/wingsoffire/procedures/MomolandhouseblockOnBlockRightClickedProcedure.java
@@ -1,7 +1,12 @@
 package net.mcreator.wingsoffire.procedures;
 
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
 public class MomolandhouseblockOnBlockRightClickedProcedure {
@@ -42,5 +47,44 @@ public class MomolandhouseblockOnBlockRightClickedProcedure {
 			zOffset = -2;
 			yOffset = yOffset + 1;
 		}
+		world.setBlock(BlockPos.containing(x + 1, y + 1, z + 2), Blocks.GLASS.defaultBlockState(), 3);
+		world.setBlock(BlockPos.containing(x + 2, y + 1, z + 2), Blocks.GLASS.defaultBlockState(), 3);
+		world.setBlock(BlockPos.containing(x - 1, y, z + 2), Blocks.AIR.defaultBlockState(), 3);
+		world.setBlock(BlockPos.containing(x - 1, y + 1, z + 2), Blocks.AIR.defaultBlockState(), 3);
+		world.setBlock(BlockPos.containing(x - 1, y + 1, z + 2), (new Object() {
+			public BlockState with(BlockState _bs, String _property, String _newValue) {
+				Property<?> _prop = _bs.getBlock().getStateDefinition().getProperty(_property);
+				return _prop instanceof EnumProperty _ep && _ep.getValue(_newValue).isPresent() ? _bs.setValue(_ep, (Enum) _ep.getValue(_newValue).get()) : _bs;
+			}
+		}.with(Blocks.OAK_DOOR.defaultBlockState(), "half", "upper")), 3);
+		world.setBlock(BlockPos.containing(x - 1, y, z + 2), Blocks.OAK_DOOR.defaultBlockState(), 3);
+		world.setBlock(BlockPos.containing(x - 1, y + 0, z - 1), (new Object() {
+			public BlockState with(BlockState _bs, Direction newValue) {
+				Property<?> _prop = _bs.getBlock().getStateDefinition().getProperty("facing");
+				if (_prop instanceof DirectionProperty _dp && _dp.getPossibleValues().contains(newValue))
+					return _bs.setValue(_dp, newValue);
+				_prop = _bs.getBlock().getStateDefinition().getProperty("axis");
+				return _prop instanceof EnumProperty _ep && _ep.getPossibleValues().contains(newValue.getAxis()) ? _bs.setValue(_ep, newValue.getAxis()) : _bs;
+			}
+		}.with((new Object() {
+			public BlockState with(BlockState _bs, String _property, String _newValue) {
+				Property<?> _prop = _bs.getBlock().getStateDefinition().getProperty(_property);
+				return _prop instanceof EnumProperty _ep && _ep.getValue(_newValue).isPresent() ? _bs.setValue(_ep, (Enum) _ep.getValue(_newValue).get()) : _bs;
+			}
+		}.with(Blocks.PINK_BED.defaultBlockState(), "part", "head")), Direction.WEST)), 3);
+		world.setBlock(BlockPos.containing(x - 0, y + 0, z - 1), (new Object() {
+			public BlockState with(BlockState _bs, Direction newValue) {
+				Property<?> _prop = _bs.getBlock().getStateDefinition().getProperty("facing");
+				if (_prop instanceof DirectionProperty _dp && _dp.getPossibleValues().contains(newValue))
+					return _bs.setValue(_dp, newValue);
+				_prop = _bs.getBlock().getStateDefinition().getProperty("axis");
+				return _prop instanceof EnumProperty _ep && _ep.getPossibleValues().contains(newValue.getAxis()) ? _bs.setValue(_ep, newValue.getAxis()) : _bs;
+			}
+		}.with((new Object() {
+			public BlockState with(BlockState _bs, String _property, String _newValue) {
+				Property<?> _prop = _bs.getBlock().getStateDefinition().getProperty(_property);
+				return _prop instanceof EnumProperty _ep && _ep.getValue(_newValue).isPresent() ? _bs.setValue(_ep, (Enum) _ep.getValue(_newValue).get()) : _bs;
+			}
+		}.with(Blocks.PINK_BED.defaultBlockState(), "part", "foot")), Direction.WEST)), 3);
 	}
 }
