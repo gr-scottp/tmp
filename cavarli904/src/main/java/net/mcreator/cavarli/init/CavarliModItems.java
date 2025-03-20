7e53b207169354f8838a2a240e50f352ff1bb56a
codeclub202316 <134312470+codeclub202316@users.noreply.github.com>
2025-03-20 17:14:59 +0000

    attempt to recover portal

--- cavarli904/src/main/java/net/mcreator/cavarli/init/CavarliModItems.java
+++ cavarli904/src/main/java/net/mcreator/cavarli/init/CavarliModItems.java
@@ -13,6 +13,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.BlockItem;
 
+import net.mcreator.cavarli.item.Lolc1234Item;
 import net.mcreator.cavarli.item.CAVARLInoob1234567890Item;
 import net.mcreator.cavarli.CavarliMod;
 
@@ -22,6 +23,7 @@ public class CavarliModItems {
 	public static final DeferredItem<Item> CAVARL_INOOB_1234567890 = REGISTRY.register("cavarl_inoob_1234567890", CAVARLInoob1234567890Item::new);
 	public static final DeferredItem<Item> OHIOTOLENOOB_1234 = block(CavarliModBlocks.OHIOTOLENOOB_1234);
 	public static final DeferredItem<Item> NOOB_1234567890_OHIO_SPAWN_EGG = REGISTRY.register("noob_1234567890_ohio_spawn_egg", () -> new DeferredSpawnEggItem(CavarliModEntities.NOOB_1234567890_OHIO, -1, -1, new Item.Properties()));
+	public static final DeferredItem<Item> LOLC_1234 = REGISTRY.register("lolc_1234", Lolc1234Item::new);
 
 	// Start of user code block custom items
 	// End of user code block custom items
--- cavarli904/src/main/java/net/mcreator/cavarli/init/CavarliModTabs.java
+++ cavarli904/src/main/java/net/mcreator/cavarli/init/CavarliModTabs.java
@@ -36,6 +36,10 @@ public class CavarliModTabs {
 
 			tabData.accept(CavarliModItems.NOOB_1234567890_OHIO_SPAWN_EGG.get());
 
+		} else if (tabData.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+
+			tabData.accept(CavarliModItems.LOLC_1234.get());
+
 		}
 	}
 }
